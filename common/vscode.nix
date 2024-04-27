bdac7bd4740111f9f55759c8e05375f77aa0c504
Christopher Miller <git@chrismiller.xyz>
2024-04-27 00:55:49 -0700

    formatted

--- common/vscode.nix
+++ common/vscode.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 
-let 
-    extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        arrterian.nix-env-selector
-        tamasfe.even-better-toml
-    ];
-in {
-    inherit extensions;
+let
+  extensions = with pkgs.vscode-extensions; [
+    bbenoist.nix
+    arrterian.nix-env-selector
+    tamasfe.even-better-toml
+  ];
+in
+{
+  inherit extensions;
 }
