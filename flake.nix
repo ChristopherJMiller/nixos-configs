7e400347b6014af3e8afd1d42dc83e6b9bed5936
Christopher Miller <hello@chrismiller.xyz>
2024-04-26 23:47:23 -0700

    reorganizing

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       rowlett = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./rowlett/configuration.nix
 
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
@@ -25,7 +25,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users.chris = import ./home.nix;
+            home-manager.users.chris = import ./rowlett/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
 	    home-manager.sharedModules = [
